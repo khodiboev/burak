c05696504b3319115cbbfc11938e0e66935c58f8
Victor <khadibayevjurabek@gmail.com>
2026-01-14 23:17:28 +0900

    feat: MITask in train.ts

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,206 @@
-console.log("play maker")
+//MIT Tasks <--H--G--F--E--D--C--B--A--------------------------------------------
+// TASK H: 
+// shunday function tuzing, u integerlardan iborat arrayni argument sifatida qabul qilib, faqat positive qiymatlarni olib string holatda return qilsin. MASALAN: getPositive([1, -4, 2]) return qiladi "12"
+
+function getPositive(arr: number[]):string {
+    let result:string = "";
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            result = result + String(arr[i]);
+        }
+    }
+
+    return result;
+}
+
+console.log(getPositive([1, -4, 2]));
+console.log(getPositive([3, -4, 5, 7, -9]));
+
+
+// ========================================================
+
+
+// TASK G:
+// Yagona parametrga ega function tuzing.
+// Va bu function parametr orqalik integer ma'lumot turlariga ega bo'lgan bir arrayni qabul qilsin.
+// Ushbu function bizga arrayning tarkibidagi birinchi eng katta qiymatning indeksini qaytarsin.
+// MASALAN: getHighestIndex([5, 21, 12, 21 ,8]); return qiladi 1 sonini
+// Yuqoridagi misolda, birinchi indeksda 21 joylashgan.
+// Va bu 21 soni arrayning tarkibidagi birinchi eng katta son hisobladi va bizga uning indeksi 1 qaytadi.
+
+// function getHighestIndex(arr) {
+//     let max = arr[0];
+//     let maxIndex = 0;
+
+//     for (let i = 1; i < arr.length; i++) {
+//         if (arr[i] > max) {
+//             max = arr[i];
+//             maxIndex = i;
+//         }
+//     }
+
+//     return maxIndex;
+// }
+// console.log(getHighestIndex([5, 21, 12, 21, 8]));
+// console.log(getHighestIndex([3, 7, 2, 1]));
+
+
+// ========================================================
+
+
+// TASK F:
+// Yagona string argumentga ega findDoublers nomli function tuzing
+// Agar stringda bittadan ortiq bir xil harflar ishtirok etgan bo'lsa
+// true yokida false natija qaytarsin. MASALAN: findDoublers("hello"); natija true qaytadi. Sababi ikki marotaba takrorlangan 'll' harfi mavjud!
+
+// function findDoublers(str) {
+//     for (let i = 0; i < str.length; i++) {
+//         for (let j = i + 1; j < str.length; j++) {
+//             if (str[i] === str[j]) {
+//                 return true;
+//             }
+//         }
+//     }
+//     return false;
+// }
+// console.log(findDoublers("hello"));
+// console.log(findDoublers("abc"));   
+// console.log(findDoublers("apple"));
+// console.log(findDoublers("world")); 
+
+
+// ========================================================
+
+
+// TASK E:
+// Shunday function tuzing, u bitta string argumentni qabul qilib osha stringni teskari qilib return qilsin.
+// MASALAN: getReverse("hello") return qilsin "olleh"
+
+// function getReverse(str) {
+//     return str.split("").reverse().join("");
+// }
+// console.log(getReverse("play"));
+// console.log(getReverse("maker"));
+
+
+// ========================================================
+
+
+// TASK-D
+// Shunday function tuzing, u 2ta string parametr ega bolsin, hamda agar har ikkala string bir hil harflardan iborat bolsa true aks holda false qaytarsin. MASALAN checkContent("mitgroup", "gmtiprou") return qiladi true;
+
+// function checkContent(str1, str2) {
+//     // Uzunlikni tekshiramiz
+//     if (str1.length !== str2.length) {
+//         return false;
+//     }
+//     // Harflarni tartiblash
+//     let sorted1 = str1.split('').sort().join('');
+//     let sorted2 = str2.split('').sort().join('');
+//     // Taqqoslash
+//     return sorted1 === sorted2;
+// }
+// console.log(checkContent("mitgroup", "gmtiprou"));
+// console.log(checkContent("hello", "ollhe"));      
+// console.log(checkContent("hello", "helloo"));     
+// console.log(checkContent("abc", "abd"));       
+
+
+// ========================================================
+
+
+// // Task-C
+// //Shunday class tuzing tuzing nomi Shop, va uni constructoriga 3 hil mahsulot pass bolsin, hamda classning 3ta methodi bolsin, biri qoldiq, biri sotish va biri qabul. Har bir method ishga tushgan vaqt ham log qilinsin. MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() return hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud! shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq() return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud!
+
+// class Shop {
+//   // Do‘kon ochilganda boshlang‘ich mahsulotlar
+//   constructor(non, lagmon, cola) {
+//     this.non = non;
+//     this.lagmon = lagmon;
+//     this.cola = cola;
+//   }
+//   // Hozirgi vaqtni olish
+//   getTime() {
+//     const now = new Date();
+//     return now.getHours() + ":" + now.getMinutes();
+//   }
+//   // Qoldiqni ko‘rsatish
+//   qoldiq() {
+//     console.log(
+//       "Hozir " + this.getTime() + "da " +
+//       this.non + "ta non, " +
+//       this.lagmon + "ta lagmon va " +
+//       this.cola + "ta cola mavjud!"
+//     );
+//   }
+//   // Mahsulot sotish
+//   sotish(mahsulot, son) {
+//     if (mahsulot === "non") {
+//       this.non = this.non - son;
+//     }
+//     if (mahsulot === "lagmon") {
+//       this.lagmon = this.lagmon - son;
+//     }
+//     if (mahsulot === "cola") {
+//       this.cola = this.cola - son;
+//     }
+//     console.log(this.getTime() + "da " + son + "ta " + mahsulot + " sotildi");
+//   }
+//   // Mahsulot qabul qilish
+//   qabul(mahsulot, son) {
+//     if (mahsulot === "non") {
+//       this.non = this.non + son;
+//     }
+//     if (mahsulot === "lagmon") {
+//       this.lagmon = this.lagmon + son;
+//     }
+//     if (mahsulot === "cola") {
+//       this.cola = this.cola + son;
+//     }
+//     console.log(this.getTime() + "da " + son + "ta " + mahsulot + " qabul qilindi");
+//   }
+// }
+
+// const shop = new Shop(4, 5, 2);
+
+// shop.qoldiq();          // Hozirgi qoldiq
+// shop.sotish("non", 3);  // 3 ta non sotildi
+// shop.qabul("cola", 4);  // 4 ta cola qabul qilindi
+// shop.qoldiq();          // Yangi qoldiq
+
+
+// ========================================================
+
+
+// Task-B
+///Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin. MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.
+
+// function countDigits(str) {
+//   let count = 0;
+//   for (let i = 0; i < str.length; i++) {
+//     if (!isNaN(str[i]) && str[i] !== ' ') {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+// console.log(countDigits("ad2a54y79we3t0sfgb9"));
+
+
+// ========================================================
+
+
+// Task-A
+//Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkinchi parametrdagi sozdan qatnashga sonini return qilishi kerak boladi.MASALAN countLetter("e", "engineer") 3ni return qiladi.
+// function countLetter(letter, word) {
+//   let count = 0;
+//   for (let i = 0; i < word.length; i++) {
+//     if (word[i] === letter) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+// console.log(countLetter('e', 'engineer'));
+// console.log(countLetter('n', 'banana'));
