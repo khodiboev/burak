5ba45288deeb714edfa10c3939b3cdcd1546f078
Victor <khadibayevjurabek@gmail.com>
2026-01-23 22:01:09 +0900

    fix: modify restaurant login and signup process

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -2,6 +2,7 @@ import MemberModel from "../schema/Member.model";
 import { LoginInput, Member, MemberInput } from "../libs/types/member";
 import Errors, { HttpCode, Message } from "../libs/errors";
 import { MemberType } from "../libs/enums/member.enum";
+import bcrypt from "bcryptjs";
 
 class MemberService {
     private readonly memberModel;
@@ -11,10 +12,15 @@ class MemberService {
     }
 
     public async processSignup(input: MemberInput): Promise<Member> {
-        const exist = await this.memberModel.findOne({memberType: MemberType.RESTAURANT}).exec();
-
-        console.log("exist:", exist);
+        const exist = await this.memberModel
+        .findOne({memberType: MemberType.RESTAURANT})
+        .exec();
         if(exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+
+        console.log("before:", input.memberPassword);
+        const salt = await bcrypt.genSalt();
+        input.memberPassword =  await bcrypt.hash(input.memberPassword, salt);
+        console.log("after:", input.memberPassword);
 
         try {
         const result = await this.memberModel.create(input);
@@ -35,7 +41,13 @@ class MemberService {
 
         if(!member) throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK);
 
-        const isMatch = member.memberPassword === input.memberPassword;
+        const isMatch = await bcrypt.compare(
+            input.memberPassword, 
+            member.memberPassword
+        );
+        // const isMatch = member.memberPassword === input.memberPassword;
+
+
 
         if(!isMatch) {
             throw new Errors(HttpCode.UNAUTHORIZED, Message.NO_MEMBER_PASSWORD);
