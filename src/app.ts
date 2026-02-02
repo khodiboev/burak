8a18d82b8b7d7ccb77bab76d8c1d717d6324c7c9
Victor <khadibayevjurabek@gmail.com>
2026-02-02 22:17:13 +0900

    getAllProducts api and passing data into ejs

--- src/app.ts
+++ src/app.ts
@@ -39,7 +39,7 @@ app.use(
 
 app.use(function(req, res, next) {
     const sessionInstance = req.session as T;
-    res.locals.session = sessionInstance.member;
+    res.locals.member = sessionInstance.member;
     next();
 });
 
