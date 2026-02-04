90f9e2a2bd858c06ce931738779774d33d386f21
Victor <khadibayevjurabek@gmail.com>
2026-02-04 16:41:02 +0900

    feat: MITask => train.js

--- src/train.ts
+++ src/train.ts
@@ -35,7 +35,24 @@
 
 
 
-//MIT Tasks <--M--L--K--J--I--H2--H--G--F--E--D--C--B--A--------------------------------------------
+//MIT Tasks <--N--M--L--K--J--I--H2--H--G--F--E--D--C--B--A---------------------------------------
+
+// TASK N:
+// Shunday function yozing, u string qabul qilsin va string palindrom yani togri oqilganda ham, orqasidan oqilganda ham bir hil oqiladigan soz ekanligini aniqlab boolean qiymat qaytarsin. MASALAN: palindromCheck("dad") return true;  palindromCheck("son") return false;
+
+function palindromCheck(word: string): boolean {
+  let reversed = word.split("").reverse().join("");
+
+  if (word === reversed) {
+
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(palindromCheck("dad"));
+console.log(palindromCheck("son"));
+
 
 // TASK M: 
 // Shunday function yozing, u raqamlardan tashkil topgan array qabul qilsin va array ichidagi har bir raqam uchun raqamni ozi va hamda osha raqamni kvadratidan tashkil topgan object hosil qilib, hosil bolgan objectlarni array ichida qaytarsin.MASALAN: getSquareNumbers([1, 2, 3]) return [{number: 1, square: 1}, {number: 2, square: 4}, {number: 3, square: 9}];
