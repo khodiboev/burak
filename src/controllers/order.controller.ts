386579067ad46321dc2978784af52a8919f24fa9
Victor <khadibayevjurabek@gmail.com>
2026-03-05 17:34:11 +0900

    feat: getMyOrders Rest API

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -3,6 +3,9 @@ import { T } from "../libs/types/common";
 import Errors, { HttpCode } from "../libs/Errors";
 import { Response } from "express";
 import OrderService from "../models/Order.service";
+import { Order } from "../libs/types/order";
+import { OrderStatus } from "../libs/enums/order.enum";
+import { OrderInquiry } from "../libs/types/order";
 
 const orderService = new OrderService();
 
@@ -12,9 +15,29 @@ orderController.createOrder = async (req: ExtendedRequest, res: Response) => {
     console.log("createOrder");
     const result = await orderService.createOrder(req.member, req.body);
 
-    res.status(HttpCode.CREATED).json({result});
+    res.status(HttpCode.CREATED).json(result);
   } catch (err) {
     console.log("Error, createOrder:", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standard.code).json(Errors.standard);
+  }
+};
+
+orderController.getMyOrders = async (req: ExtendedRequest, res: Response) => {
+  try {
+    console.log("getMyOrders");
+    const {page, limit, orderStatus} = req.query;
+    const inquiry: OrderInquiry = {
+      page: Number(page),
+      limit: Number(limit),
+      orderStatus: orderStatus as OrderStatus
+    };
+    console.log("inquiry", inquiry);
+    const result = await orderService.getMyOrders(req.member, inquiry);
+
+    res.status(HttpCode.OK).json(result);
+  } catch (err) {
+    console.log("Error, getMyOrders:", err);
     if (err instanceof Errors) res.status(err.code).json(err);
     else res.status(Errors.standard.code).json(Errors.standard);
   }
--- src/router.ts
+++ src/router.ts
@@ -58,4 +58,10 @@ router.post(
   orderController.createOrder,
 );
 
+router.get(
+  "/order/all",
+  memberController.verifyAuth,
+  orderController.getMyOrders,
+);
+
 export default router;
