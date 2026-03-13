b77d63ee789aa207ce935bf60ff73c5b65511dc5
Victor <khadibayevjurabek@gmail.com>
2026-03-13 18:23:45 +0900

    feat: mitask

--- src/train.ts
+++ src/train.ts
@@ -35,6 +35,25 @@
 
 // -----------> MIT TASKS:
 
+// TASK ZD
+// Shunday function yozing. Bu function o'ziga, parametr sifatida
+// birinchi oddiy number, keyin yagona array va uchinchi bo'lib oddiy number
+// qabul qilsin. Berilgan birinchi number parametr, arrayning tarkibida indeks bo'yicha hisoblanib,
+// shu aniqlangan indeksni uchinchi number parametr bilan alashtirib, natija sifatida
+// yangilangan arrayni qaytarsin.
+// MASALAN: changeNumberInArray(1, [1,3,7,2], 2) return [1,2,7,2];
+// Yuqoridagi misolda, birinchi raqam bu '1' va arrayning '1'chi indeksi bu 3.
+// Bizning function uchinchi berilgan '2' raqamini shu '3' bilan almashtirib,
+// yangilangan arrayni qaytarmoqda.
+
+function changeNumberInArray(index: number, arr: number[], newValue: number): number[] {
+  if (index >= 0 && index < arr.length) {
+    arr[index] = newValue;}
+  return arr;
+}
+console.log(changeNumberInArray(1, [1, 3, 7, 2], 2));
+
+
 // TASK ZC
 // Selisy (°C) shkalasi bo'yicha raqam qabul qilib, uni
 // Ferenhayt (°F) shkalisaga o'zgaritib beradigan function yozing.
@@ -44,11 +63,11 @@
 // Yoki 10 gradus Selsiy, 50 Farenhaytga teng.
 // °C va °F => Tempraturani o'lchashda ishlatiladigan o'lchov birligi.
 
-function celsiusToFahrenheit(celsius: number): number {
-  return (celsius * 9) / 5 + 32;
-}
-console.log(celsiusToFahrenheit(0));
-console.log(celsiusToFahrenheit(10));
+// function celsiusToFahrenheit(celsius: number): number {
+//   return (celsius * 9) / 5 + 32;
+// }
+// console.log(celsiusToFahrenheit(0));
+// console.log(celsiusToFahrenheit(10));
 
 
 // TASK-ZB:
